0bc7249bf9f46b06dc46fda25e2410e77e992578
milan.jokanovic <milan.jokanovic98@gmail.com>
2020-12-02 18:03:43 +0100

    Task 11 inital commit

--- server/src/main/java/com/culturaloffers/maps/controllers/OfferTypeController.java
+++ server/src/main/java/com/culturaloffers/maps/controllers/OfferTypeController.java
@@ -1,9 +1,174 @@
 package com.culturaloffers.maps.controllers;
 
+import com.culturaloffers.maps.dto.OfferTypeDTO;
+import com.culturaloffers.maps.model.OfferType;
+import com.culturaloffers.maps.model.Subtype;
+import com.culturaloffers.maps.services.OfferTypeService;
+import com.culturaloffers.maps.services.SubtypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/offer-types")
 public class OfferTypeController {
+    @Autowired
+    private OfferTypeService offerTypeService;
+
+    @Autowired
+    private SubtypeService subtypeService;
+
+    //private CulturalContentCategoryMapper culturalContentCategoryMapper;
+
+    //private CategoryTypeMapper categoryTypeMapper;
+
+    //@Autowired
+    //private CategoryTypeService categoryTypeService;
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<OfferTypeDTO>> getAllOfferTypes() {
+        List<OfferType> offerTypes = offerTypeService.findAll();
+        List<String> subtypes = new ArrayList<String>();
+        List<OfferTypeDTO> offerTypeDTOS = new ArrayList<OfferTypeDTO>();
+        for(OfferType offerType : offerTypes){
+            for(Subtype subtype : offerType.getSubtypes()){
+                subtypes.add(subtype.getName());
+            }
+            OfferTypeDTO dto = new OfferTypeDTO(offerType.getId(), offerType.getName(), subtypes);
+            offerTypeDTOS.add(dto);
+            subtypes = new ArrayList<String>();
+
+        }
+        return new ResponseEntity<>(offerTypeDTOS, HttpStatus.OK);
+    }
+
+    @RequestMapping(value= "/by-page", method = RequestMethod.GET)
+    public ResponseEntity<Page<OfferTypeDTO>> getAllOfferTypes(Pageable pageable) {
+        Page<OfferType> offerTypesPage = offerTypeService.findAll(pageable);
+        List<String> subtypes = new ArrayList<String>();
+        List<OfferTypeDTO> offerTypeDTOS = new ArrayList<OfferTypeDTO>();
+        for(OfferType offerType : offerTypesPage){
+            for(Subtype subtype : offerType.getSubtypes()){
+                subtypes.add(subtype.getName());
+            }
+            OfferTypeDTO dto = new OfferTypeDTO(offerType.getId(), offerType.getName(), subtypes);
+            offerTypeDTOS.add(dto);
+            subtypes = new ArrayList<String>();
+
+        }
+
+        return new ResponseEntity<>(new PageImpl<>(offerTypeDTOS,offerTypesPage.getPageable(),offerTypesPage.getTotalElements()), HttpStatus.OK);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value="/{name}", method=RequestMethod.GET)
+    public ResponseEntity<OfferTypeDTO> getOfferType(@PathVariable String name){
+        OfferType offerType = offerTypeService.findOne(name);
+        if(offerType == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        List<String> subtypes = new ArrayList<String>();
+        for(Subtype subtype : offerType.getSubtypes()){
+            subtypes.add(subtype.getName());
+        }
+        OfferTypeDTO dto = new OfferTypeDTO(offerType.getId(), offerType.getName(), subtypes);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<OfferTypeDTO> createOfferType(@RequestBody OfferTypeDTO offerTypeDTO){
+        OfferType offerType;
+        try {
+            offerType = new OfferType();
+            offerType.setName(offerTypeDTO.getName());
+
+            //offerType = offerTypeService.create(offerType);
+            Set<Subtype> subtypes = new HashSet<Subtype>();
+            for(String type : offerTypeDTO.getSubtypes()){
+                Subtype subtype = new Subtype();
+                subtype.setName(type);
+                subtype.setOfferType(offerType);
+                //subtypeService.create(subtype, offerType.getName());
+                subtypes.add(subtype);
+            }
+            offerType.setSubtypes(subtypes);
+            offerType = offerTypeService.create(offerType);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        List<String> subtypes = new ArrayList<String>();
+        for(Subtype subtype : offerType.getSubtypes()){
+            subtypes.add(subtype.getName());
+        }
+        OfferTypeDTO dto = new OfferTypeDTO(offerType.getId(), offerType.getName(), subtypes);
+
+        return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<OfferTypeDTO> updateOfferType(
+            @RequestBody OfferTypeDTO offerTypeDTO, @PathVariable Integer id){
+        OfferType offerType;
+        try {
+            offerType = new OfferType();
+            offerType.setName(offerTypeDTO.getName());
+            offerType = offerTypeService.update(offerType, id);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        List<String> subtypes = new ArrayList<String>();
+        for(Subtype subtype : offerType.getSubtypes()){
+            subtypes.add(subtype.getName());
+        }
+        OfferTypeDTO dto = new OfferTypeDTO(offerType.getId(), offerType.getName(), subtypes);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value="/{name}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteOfferType(@PathVariable String name){
+        try {
+            offerTypeService.delete(name);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+   /* public CulturalContentCategoryController() {
+
+        culturalContentCategoryMapper = new CulturalContentCategoryMapper();
+        categoryTypeMapper = new CategoryTypeMapper();
+    }
+
+    private List<CulturalContentCategoryDTO> toCulturalContentCategoryDTOList(List<CulturalContentCategory> culturalContentCategories){
+        List<CulturalContentCategoryDTO> culturalContentCategoryDTOS = new ArrayList<>();
+        for (CulturalContentCategory culturalContentCategory: culturalContentCategories) {
+            culturalContentCategoryDTOS.add(culturalContentCategoryMapper.toDto(culturalContentCategory));
+        }
+        return culturalContentCategoryDTOS;
+    }
+
+    private List<CategoryTypeDTO> toCategoryTypeDTOList(List<CategoryType> categoryTypes) {
+        List<CategoryTypeDTO> categoryTypeDTOS = new ArrayList<>();
+        for (CategoryType categoryType: categoryTypes) {
+            categoryTypeDTOS.add(categoryTypeMapper.toDto(categoryType));
+        }
+        return categoryTypeDTOS;
+    }*/
 }
--- server/src/main/java/com/culturaloffers/maps/controllers/SubtypeController.java
+++ server/src/main/java/com/culturaloffers/maps/controllers/SubtypeController.java
@@ -1,9 +1,120 @@
 package com.culturaloffers.maps.controllers;
 
+import com.culturaloffers.maps.dto.OfferTypeDTO;
+import com.culturaloffers.maps.dto.SubtypeDTO;
+import com.culturaloffers.maps.model.OfferType;
+import com.culturaloffers.maps.model.Subtype;
+import com.culturaloffers.maps.services.OfferTypeService;
+import com.culturaloffers.maps.services.SubtypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/subtypes")
 public class SubtypeController {
+
+    @Autowired
+    private SubtypeService subtypeService;
+
+    //private CulturalContentCategoryMapper culturalContentCategoryMapper;
+
+    //private CategoryTypeMapper categoryTypeMapper;
+
+    //@Autowired
+    //private CategoryTypeService categoryTypeService;
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<SubtypeDTO>> getAllSubtypes() {
+        List<Subtype> subtypes = subtypeService.findAll();
+        List<SubtypeDTO> subtypeDTOS = new ArrayList<SubtypeDTO>();
+        for(Subtype subtype : subtypes){
+            SubtypeDTO subtypeDTO = new SubtypeDTO(subtype.getId(), subtype.getName(), subtype.getOfferType().getName());
+            subtypeDTOS.add(subtypeDTO);
+        }
+        return new ResponseEntity<>(subtypeDTOS, HttpStatus.OK);
+    }
+
+    @RequestMapping(value= "/by-page", method = RequestMethod.GET)
+    public ResponseEntity<Page<SubtypeDTO>> getAllSubtypes(Pageable pageable) {
+        Page<Subtype> subtypesPage = subtypeService.findAll(pageable);
+        List<SubtypeDTO> subtypeDTOS = new ArrayList<SubtypeDTO>();
+        for(Subtype subtype : subtypesPage){
+            SubtypeDTO subtypeDTO = new SubtypeDTO(subtype.getId(), subtype.getName(), subtype.getOfferType().getName());
+            subtypeDTOS.add(subtypeDTO);
+        }
+        return new ResponseEntity<>(new PageImpl<>(subtypeDTOS,subtypesPage.getPageable(),subtypesPage.getTotalElements()), HttpStatus.OK);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value="/{name}", method=RequestMethod.GET)
+    public ResponseEntity<SubtypeDTO> getSubype(@PathVariable String name){
+        Subtype subtype = subtypeService.findOne(name);
+        if(subtype == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        SubtypeDTO subtypeDTO = new SubtypeDTO(subtype.getId(), subtype.getName(), subtype.getOfferType().getName());
+        return new ResponseEntity<>(subtypeDTO, HttpStatus.OK);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SubtypeDTO> createSubtype(@RequestBody SubtypeDTO subtypeDTO){
+        Subtype subtype;
+        try {
+            subtype = new Subtype();
+            subtype.setName(subtypeDTO.getName());
+
+            subtype = subtypeService.create(subtype, subtypeDTO.getOfferTypeName());
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        SubtypeDTO newSubtypeDTO = new SubtypeDTO(subtype.getId(), subtype.getName(), subtype.getOfferType().getName());
+
+        return new ResponseEntity<>(newSubtypeDTO, HttpStatus.CREATED);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SubtypeDTO> updateSubtype(
+            @RequestBody SubtypeDTO subtypeDTO, @PathVariable Integer id){
+        Subtype subtype;
+        try {
+            subtype = new Subtype();
+            subtype.setName(subtypeDTO.getName());
+            subtype = subtypeService.update(subtype, id);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        SubtypeDTO newSubtypeDTO = new SubtypeDTO(subtype.getId(), subtype.getName(), subtype.getOfferType().getName());
+        return new ResponseEntity<>(newSubtypeDTO, HttpStatus.OK);
+    }
+
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value="/{name}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteSubtype(@PathVariable String name){
+        try {
+            subtypeService.delete(name);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
