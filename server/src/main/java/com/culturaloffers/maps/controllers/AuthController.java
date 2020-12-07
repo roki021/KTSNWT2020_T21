f72d7999956c9ae26f986e34cbc394733b6808d5
roki021 <26367195+roki021@users.noreply.github.com>
2020-12-07 23:59:29 +0100

    Implemented login REST point and with token authorization. #2.2

---
+++ server/src/main/java/com/culturaloffers/maps/controllers/AuthController.java
@@ -0,0 +1,49 @@
+package com.culturaloffers.maps.controllers;
+
+import com.culturaloffers.maps.dto.UserLoginDTO;
+import com.culturaloffers.maps.dto.UserTokenStateDTO;
+import com.culturaloffers.maps.model.User;
+import com.culturaloffers.maps.security.TokenUtils;
+import com.culturaloffers.maps.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserTokenStateDTO> createAuthenticationToken(@RequestBody UserLoginDTO authenticationRequest,
+                                                                       HttpServletResponse response) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()));
+
+        // Ubaci korisnika u trenutni security kontekst
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Kreiraj token za tog korisnika
+        User user = (User) authentication.getPrincipal();
+        String jwt = tokenUtils.generateToken(user.getUsername());
+        int expiresIn = tokenUtils.getExpiredIn();
+
+        // Vrati token kao odgovor na uspesnu autentifikaciju
+        return ResponseEntity.ok(new UserTokenStateDTO(jwt, expiresIn));
+    }
+}
--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -2,26 +2,28 @@ INSERT INTO authority (name) VALUES
     ('ROLE_ADMIN'),
     ('ROLE_GUEST');
 
+-- password for all users is '12345'
 INSERT INTO guest (id, first_name, last_name, email_address, username, password, active) VALUES
-    (1001, 'Petar', 'Petrović', 'ppetrovic@gmail.com', 'perica', '12345', true),
-    (1002, 'Mika', 'Mikić', 'mmikic@gmail.com', 'mikica', '12345', true),
-    (1003, 'Bogdan', 'Bogdanović', 'bbogdan@gmail.com', 'bogi', '12345', true),
-    (1004, 'Mika', 'Mikić', 'mkmik@gmail.com', 'mikarije', '12345', false),
-    (1005, 'Goran', 'Petrović', 'gpetrovic@gmail.com', 'goPet', '54321', null),
-    (1006, 'Jovana', 'Mihajlović', 'jmihail@gmail.com', 'jovana25', '12345', true),
-    (1007, 'Milana', 'Rakić', 'mrakic@gmail.com', 'rakica2', '12345', true),
-    (1008, 'Zorana', 'Zoranić', 'zzora@gmail.com', 'zoka61', '12345', true),
-    (1009, 'Gorana', 'Maksimović', 'gmax2@gmail.com', 'gorama77', '12345', true),
-    (1010, 'Kosta', 'Kostić', 'kkostic@gmail.com', 'kostica1', '12345', false),
-    (1011, 'Uroš', 'Bogdanović', 'urosb@gmail.com', 'urosNejaki', '12345', true),
-    (1012, 'Milica', 'Nikolić', 'micani@gmail.com', 'mica51', '12345', true),
-    (1013, 'Petar', 'Jokić', 'pjoka@gmail.com', 'joka261', '54321', null),
-    (1014, 'Gorana', 'Jeftović', 'gojefta@gmail.com', 'goraje2', '12345', true),
-    (1015, 'Vladimir', 'Rakić', 'vladar2@gmail.com', 'vladar21', '12345', true),
-    (1016, 'Zorana', 'Zoranić', 'zers61@gmail.com', 'zorat2', '12345', true);
+    (1001, 'Petar', 'Petrović', 'ppetrovic@gmail.com', 'perica', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1002, 'Mika', 'Mikić', 'mmikic@gmail.com', 'mikica', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1003, 'Bogdan', 'Bogdanović', 'bbogdan@gmail.com', 'bogi', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1004, 'Mika', 'Mikić', 'mkmik@gmail.com', 'mikarije', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', false),
+    (1005, 'Goran', 'Petrović', 'gpetrovic@gmail.com', 'goPet', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', null),
+    (1006, 'Jovana', 'Mihajlović', 'jmihail@gmail.com', 'jovana25', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1007, 'Milana', 'Rakić', 'mrakic@gmail.com', 'rakica2', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1008, 'Zorana', 'Zoranić', 'zzora@gmail.com', 'zoka61', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1009, 'Gorana', 'Maksimović', 'gmax2@gmail.com', 'gorama77', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1010, 'Kosta', 'Kostić', 'kkostic@gmail.com', 'kostica1', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', false),
+    (1011, 'Uroš', 'Bogdanović', 'urosb@gmail.com', 'urosNejaki', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1012, 'Milica', 'Nikolić', 'micani@gmail.com', 'mica51', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1013, 'Petar', 'Jokić', 'pjoka@gmail.com', 'joka261', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', null),
+    (1014, 'Gorana', 'Jeftović', 'gojefta@gmail.com', 'goraje2', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1015, 'Vladimir', 'Rakić', 'vladar2@gmail.com', 'vladar21', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true),
+    (1016, 'Zorana', 'Zoranić', 'zers61@gmail.com', 'zorat2', '$2a$04$iLVT9N/5RKaS1bMXEmueauu7pU1ZROAxtRT0x8pAGGuQtmp9E8LH.', true);
 
-INSERT INTO admin (id, username, password) VALUES
-    (1111, 'admin', 'admin');
+-- password for admin is 'admin'
+INSERT INTO admin (id, username, password, active) VALUES
+    (1111, 'admin', '$2a$04$5gn/3csNiz5C9S8E5SI.IO9gi8WF6AHofzUW0Ynk3.V2BzTu0sbGG', true);
 
 INSERT INTO user_authority (user_id, authority_id) VALUES
     (1111, 1),
