71e4f4d8c2e3e6f1e79676d7ab50d841734c6533
senpai132 <acanikolic021@gmail.com>
2020-12-10 01:00:04 +0100

    Fixed image upload

--- server/src/main/java/com/culturaloffers/maps/config/WebConfiguration.java
+++ server/src/main/java/com/culturaloffers/maps/config/WebConfiguration.java
@@ -20,12 +20,13 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/public/images/commentImages/")
-                .addResourceLocations("classpath:/public/images/newsImages/")
-                .addResourceLocations("classpath:/public/images/offerImages/")
+                .addResourceLocations("file:src/main/images/commentImages/")
+                .addResourceLocations("file:src/main/images/newsImages/")
+                .addResourceLocations("file:src/main/images/offerImages/")
                 .setCachePeriod(1000)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
     }
+
 }
