2f00987749e9ebcc8d46fff3dbface23fd45f6b8
JSTheGreat <jovansvorcan14@gmail.com>
2020-12-25 18:24:36 +0100

    Small shanges made to CulturalOfferControllerTest

--- server/src/test/java/com/culturaloffers/maps/controllers/CulturalOfferControllerTest.java
+++ server/src/test/java/com/culturaloffers/maps/controllers/CulturalOfferControllerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.culturaloffers.maps.dto.CulturalOfferDTO;
 import com.culturaloffers.maps.dto.UserLoginDTO;
+import com.culturaloffers.maps.dto.UserTokenStateDTO;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,19 +26,34 @@ public class CulturalOfferControllerTest {
 
     private String accessToken;
 
+    private HttpHeaders httpHeaders;
+
     @Before
     public void login() {
-        ResponseEntity<String> responseEntity =
-                restTemplate.postForEntity("/auth/login",
-                        new UserLoginDTO("admin", "admin"),
-                        String.class);
-        accessToken = responseEntity.getBody();
+        ResponseEntity<UserTokenStateDTO> responseEntity = restTemplate.postForEntity("/auth/login",
+                new UserLoginDTO("admin","admin"), UserTokenStateDTO.class);
+        accessToken = "Bearer " + responseEntity.getBody().getAccessToken();
+        httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", accessToken);
     }
 
     @Test
     public void testGetAll(){
+        HttpEntity<Object> httpEntity = new HttpEntity<Object>(httpHeaders);
+        ResponseEntity<CulturalOfferDTO[]> responseEntity =
+                restTemplate.exchange("/offers", HttpMethod.GET, httpEntity, CulturalOfferDTO[].class);
+        CulturalOfferDTO[] dtos = responseEntity.getBody();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(dtos).isNotNull();
+        assertThat(dtos).isNotEmpty();
+        assertThat(dtos.length).isEqualTo(15);
+    }
+
+    @Test
+    public void testGetAllPageable(){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Token", accessToken);
+
         HttpEntity<Object> httpEntity = new HttpEntity<Object>(headers);
         ResponseEntity<CulturalOfferDTO[]> responseEntity =
                 restTemplate.exchange("/offers", HttpMethod.GET, httpEntity, CulturalOfferDTO[].class);
