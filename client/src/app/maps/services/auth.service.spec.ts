a1873cea0938e048fbc1dcba3beccd6991eadf5b
roki021 <26367195+roki021@users.noreply.github.com>
2021-01-26 23:12:19 +0100

    Finished auth service test on angular side. #1 #2

--- client/src/app/maps/services/auth.service.spec.ts
+++ client/src/app/maps/services/auth.service.spec.ts
@@ -85,6 +85,14 @@ describe('AuthService', () => {
 
     authService.login('perica', '12345').subscribe(response => {
       loggedIn = response;
+      const user = {
+        id: 1001,
+        username: 'perica',
+        expireIn: new Date().getTime() + 18000000,
+        authorities: ['ROLE_GUEST'],
+        token: 'token'
+      };
+      localStorage.setItem('user', JSON.stringify(user));
     });
 
     const req = httpMock.expectOne('api/auth/login');
@@ -93,13 +101,54 @@ describe('AuthService', () => {
 
     tick();
     userToken = authService.getCurrentUser();
+    expect(loggedIn).toEqual(true);
     expect(userToken).toBeDefined();
     expect(userToken.id).toEqual(1001);
     expect(userToken.username).toEqual('perica');
     expect(userToken.authorities).toEqual(['ROLE_GUEST']);
   }));
 
-  /*it('validateToken() checks token for expiration and log out user', fakeAsync(() => {
+  it('login() log in user with wrong credentials', fakeAsync(() => {
+    let loggedIn: boolean;
+
+    authService.login('perica', '25251').subscribe(
+      response => {
+        loggedIn = response;
+      },
+      error => {
+        loggedIn = false;
+      }
+    );
+
+    const req = httpMock.expectOne('api/auth/login');
+    expect(req.request.method).toBe('POST');
+    req.flush({}, { status: 401, statusText: 'Unauthorized' });
+
+    tick();
+    expect(authService.isLoggedIn()).toEqual(false);
+    expect(loggedIn).toEqual(false);
+  }));
+
+  it('validateToken() checks token for expiration and log out user', fakeAsync(() => {
+    const user = {
+      id: 1001,
+      username: 'perica',
+      expireIn: new Date('2020-10-11T10:20:30Z').getTime() + 18000000,
+      authorities: ['ROLE_GUEST'],
+      token: 'token'
+    };
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    expect(authService.isLoggedIn()).toEqual(true);
+
+    authService.validateToken();
+
+    tick();
+    expect(authService.isLoggedIn()).toEqual(false);
+  }));
+
+  it('validateToken() checks token for expiration and do nothing', fakeAsync(() => {
     const user = {
       id: 1001,
       username: 'perica',
@@ -107,7 +156,52 @@ describe('AuthService', () => {
       authorities: ['ROLE_GUEST'],
       token: 'token'
     };
+    
     localStorage.setItem('user', JSON.stringify(user));
-  }));*/
 
+    expect(authService.isLoggedIn()).toEqual(true);
+
+    authService.validateToken();
+
+    tick();
+    expect(authService.isLoggedIn()).toEqual(true);
+  }));
+
+  it('refreshToken() refreshes user token', fakeAsync(() => {
+    let userToken: UserToken;
+    let refreshed: boolean;
+    const mockUserToken = {
+      accessToken: `
+        eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJjdWx0dXJhbC1vZmZlcnMtbWFwIiwic3ViIjoicGVyaWNhIi
+        widXNlcl9pZCI6IjEwMDEiLCJyb2xlcyI6W3siaWQiOjIsIm5hbWUiOiJST0xFX0dVRVNUIiwiYXV0aG9yaXR5IjoiUk9M
+        RV9HVUVTVCJ9XSwiYXVkIjoid2ViIiwiaWF0IjoxNjExNjg5ODI2LCJleHAiOjE2MTE3MDc4MjZ9.5DEycxkIHT-vNvrcADsmsPT0ckkFk0n
+        aySTPwt4nS7mSNIQWIiCNGt0VUaSWrdSLQDBnLqAzFVVSdzDLikOuOw
+      `,
+      expiresIn: 18000000
+    };
+
+    authService.refreshToken().subscribe(response => {
+      refreshed = response;
+      const user = {
+        id: 1001,
+        username: 'perica',
+        expireIn: new Date().getTime() + 18000000,
+        authorities: ['ROLE_GUEST'],
+        token: 'token'
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+    });
+
+    const req = httpMock.expectOne('api/auth/refresh');
+    expect(req.request.method).toBe('POST');
+    req.flush(mockUserToken);
+
+    tick();
+    userToken = authService.getCurrentUser();
+    expect(refreshed).toEqual(true);
+    expect(userToken).toBeDefined();
+    expect(userToken.id).toEqual(1001);
+    expect(userToken.username).toEqual('perica');
+    expect(userToken.authorities).toEqual(['ROLE_GUEST']);
+  }));
 });
