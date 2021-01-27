eaa2a795e9675d7161866d8ca4b41705b7264c4b
roki021 <26367195+roki021@users.noreply.github.com>
2021-01-27 15:42:53 +0100

    Added tests for confirm registration page

--- client/src/app/maps/confirm-reg/confirm-reg.component.spec.ts
+++ client/src/app/maps/confirm-reg/confirm-reg.component.spec.ts
@@ -1,25 +1,73 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { RegistrationService } from '../services/registration.service';
 
 import { ConfirmRegComponent } from './confirm-reg.component';
 
 describe('ConfirmRegComponent', () => {
   let component: ConfirmRegComponent;
   let fixture: ComponentFixture<ConfirmRegComponent>;
+  let regService: RegistrationService;
+  let activeRoute: ActivatedRoute;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ConfirmRegComponent ]
-    })
-    .compileComponents();
-  }));
+    const regServiceMock = {
+      confirmRegistration: jasmine.createSpy('confirmRegistration')
+        .and.returnValue(of(true))
+    };
 
-  beforeEach(() => {
+    const activeRouteMock = {
+      queryParams: of({token: ''})
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [ ConfirmRegComponent ],
+      providers: [
+        {
+          provide: RegistrationService, useValue: regServiceMock
+        },
+        {
+          provide: ActivatedRoute, useValue: activeRouteMock
+        }
+      ]
+    });
+
     fixture = TestBed.createComponent(ConfirmRegComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    regService = TestBed.inject(RegistrationService);
+    activeRoute = TestBed.inject(ActivatedRoute);
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should show successful verification message', async(() => {
+    component.ngOnInit();
+    expect(regService.confirmRegistration).toHaveBeenCalled();
+
+    fixture.whenStable()
+      .then(() => {
+        expect(component.isVerified).toBe(true);
+        fixture.detectChanges();
+        const element: DebugElement =
+          fixture.debugElement.query(By.css('a'));
+        expect(element).toBeDefined();
+      });
+  }));
+
+  it('should show unsuccessful verification message', async(() => {
+    component.ngOnInit();
+    expect(regService.confirmRegistration).toHaveBeenCalled();
+    component.isVerified = false;
+    component.badMessage = 'message';
+
+    fixture.whenStable()
+      .then(() => {
+        expect(component.isVerified).toBe(false);
+        fixture.detectChanges();
+        const element: DebugElement =
+          fixture.debugElement.query(By.css('h3'));
+        expect(element).toBeDefined();
+      });
+  }));
 });
