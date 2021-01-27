3aa5a59c0330fcc2a2f2fa3d816742f51932a180
JSTheGreat <jovansvorcan14@gmail.com>
2021-01-27 01:24:27 +0100

    Small changes made to frontend of offer news table

--- server/src/test/java/com/culturaloffers/maps/e2e/pages/OfferNewsPage.java
+++ server/src/test/java/com/culturaloffers/maps/e2e/pages/OfferNewsPage.java
@@ -1,2 +1,101 @@
-package com.culturaloffers.maps.e2e.pages;public class OfferNewsPage {
+package com.culturaloffers.maps.e2e.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class OfferNewsPage {
+
+    private WebDriver driver;
+
+    @FindBy(xpath = "//*[@id=\"title\"]")
+    private WebElement title;
+
+    @FindBy(xpath = "//*[@id=\"description\"]")
+    private WebElement description;
+
+    @FindBy(xpath = "//*[text()=\"Add\"]")
+    private WebElement addBtn;
+
+    @FindBy(xpath = "//*[text()=\"Update\"]")
+    private WebElement updateBtn;
+
+    @FindBy(xpath = "//*[@class=\"btn\"]//following::button[2]")
+    private WebElement openViewTableBtn;
+
+    @FindBy(xpath = "//*[@class=\"btn\"]")
+    private WebElement openUpdateBtn;
+
+    @FindBy(xpath = "//*[text()=\"Create new\"]")
+    private WebElement createBtn;
+
+    @FindBy(xpath = "//*[@aria-label=\"Next\"]")
+    private WebElement nextPage;
+
+    @FindBy(xpath = "//*[@class=\"btn\"]//following::button[1]")
+    private WebElement deleteBtn;
+
+    public void ensureIsDisplayedOfferNewsTable() {
+        (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.id("news")));
+    }
+
+    public void ensureAlertAppeared() {
+        (new WebDriverWait(driver, 40)).until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void ensureIsDisplayedUpdatedOfferNews(String newName){
+        this.driver.switchTo().alert().accept();
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.numberOfElementsToBe(By.xpath("//*[text()=\"" + newName + "\"]"),1));
+    }
+
+    public void ensureDeletionHappened(String oldNews){
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.numberOfElementsToBe(By.xpath("//*[text()=\"" + oldNews + "\"]"),0));
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebElement getTitle() {
+        return title;
+    }
+
+    public WebElement getDescription() {
+        return description;
+    }
+
+    public WebElement getAddBtn() {
+        return addBtn;
+    }
+
+    public WebElement getUpdateBtn() {
+        return updateBtn;
+    }
+
+    public WebElement getOpenUpdateBtn() {
+        return openUpdateBtn;
+    }
+
+    public WebElement getCreateBtn() {
+        return createBtn;
+    }
+
+    public WebElement getNextPage() {
+        return nextPage;
+    }
+
+    public WebElement getDeleteBtn() {
+        return deleteBtn;
+    }
+
+    public OfferNewsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public OfferNewsPage() {
+    }
+
 }
--- server/src/test/java/com/culturaloffers/maps/e2e/tests/OfferNewsTableCRUDE2ETest.java
+++ server/src/test/java/com/culturaloffers/maps/e2e/tests/OfferNewsTableCRUDE2ETest.java
@@ -1,2 +1,187 @@
-package com.culturaloffers.maps.e2e.tests;public class OfferNewsTableCRUDE2ETest {
+package com.culturaloffers.maps.e2e.tests;
+
+import com.culturaloffers.maps.e2e.pages.LoginPage;
+import com.culturaloffers.maps.e2e.pages.MainPage;
+import com.culturaloffers.maps.e2e.pages.OfferNewsPage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource("classpath:test.properties")
+public class OfferNewsTableCRUDE2ETest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private MainPage mainPage;
+    private OfferNewsPage offerNewsPage;
+    public static final String BASE_URL = "http://localhost:4200";
+
+    @Before
+    public void setUp() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+        mainPage = PageFactory.initElements(driver, MainPage.class);
+        offerNewsPage = PageFactory.initElements(driver, OfferNewsPage.class);
+
+        driver.get(BASE_URL + "/login");
+
+        justWait();
+
+        loginPage.getUsername().sendKeys("admin");
+
+        loginPage.getPassword().sendKeys("admin");
+
+        justWait();
+
+        loginPage.getLoginBtn().click();
+
+        loginPage.ensureIsNotVisibleLoginBtn();
+
+        mainPage.ensureIsDisplayedMap();
+
+        justWait();
+
+        driver.navigate().to(BASE_URL + "/news/2");
+    }
+
+//    @Test
+//    public void AddOfferNewsSuccess() throws InterruptedException {
+//        offerNewsPage.ensureIsDisplayedOfferNewsTable();
+//        assertEquals("http://localhost:4200/news/2", driver.getCurrentUrl());
+//
+//        justWait();
+//
+//        offerNewsPage.getAddBtn().click();
+//
+//        justWait();
+//
+//        offerNewsPage.getTitle().sendKeys("new title");
+//
+//        offerNewsPage.getDescription().sendKeys("new desc");
+//
+//        justWait();
+//
+//        offerNewsPage.getCreateBtn().click();
+//
+//        justWait();
+//        justWait();
+//        justWait();
+//        justWait();
+//
+//        offerNewsPage.ensureAlertAppeared();
+//
+//        justWait();
+//        justWait();
+//
+//        assertEquals(offerNewsPage.getDriver().switchTo().alert().getText(), "Offer news created!");
+//
+//        justWait();
+//        justWait();
+//    }
+
+    @Test
+    public void AddOfferNewsFail() throws InterruptedException {
+        offerNewsPage.ensureIsDisplayedOfferNewsTable();
+        assertEquals("http://localhost:4200/news/2", driver.getCurrentUrl());
+
+        justWait();
+
+        offerNewsPage.getAddBtn().click();
+
+        justWait();
+
+        offerNewsPage.getTitle().sendKeys("");
+
+        offerNewsPage.getDescription().sendKeys("new desc");
+
+        justWait();
+
+        offerNewsPage.getCreateBtn().click();
+
+        justWait();
+        justWait();
+
+        offerNewsPage.ensureAlertAppeared();
+
+        assertEquals(offerNewsPage.getDriver().switchTo().alert().getText(), "Some fields are empty or invalid!");
+
+        justWait();
+        justWait();
+        justWait();
+    }
+
+    @Test
+    public void UpdateCulturalOffer() throws InterruptedException {
+        offerNewsPage.ensureIsDisplayedOfferNewsTable();
+        assertEquals("http://localhost:4200/news/2", driver.getCurrentUrl());
+        String newTitle = "updated Title";
+        justWait();
+
+        offerNewsPage.getOpenUpdateBtn().click();
+
+        justWait();
+        offerNewsPage.getTitle().clear();
+        offerNewsPage.getTitle().sendKeys(newTitle);
+
+        justWait();
+
+        offerNewsPage.getUpdateBtn().click();
+
+        justWait();
+        justWait();
+
+        offerNewsPage.ensureIsDisplayedUpdatedOfferNews(newTitle);
+
+        justWait();
+        justWait();
+        justWait();
+    }
+
+    @Test
+    public void DeleteCulturalOffer() throws InterruptedException {
+        offerNewsPage.ensureIsDisplayedOfferNewsTable();
+        assertEquals("http://localhost:4200/news/2", driver.getCurrentUrl());
+        justWait();
+
+        offerNewsPage.getDeleteBtn().click();
+
+        justWait();
+        justWait();
+
+        String oldNews = "Ponovno otvaranje parka";
+        justWait();
+        offerNewsPage.ensureDeletionHappened(oldNews);
+
+        justWait();
+        justWait();
+        justWait();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private void justWait() throws InterruptedException {
+        synchronized (driver)
+        {
+            driver.wait(1000);
+        }
+    }
+
 }
